45f1409f4a96b60690b1af1bb571586072afca4c
phuong-hodfords <phuong.vt@hodfords.com>
2024-06-05 16:46:36 +0700

    fix: infinity loop import

--- libs/types/relation-group.type.ts
+++
@@ -1,7 +0,0 @@
-import { SelectQueryBuilder } from 'typeorm';
-
-export type RelationGroupType = {
-    level: number;
-    name: string;
-    customQuery?: (name: SelectQueryBuilder<any>) => void;
-};
