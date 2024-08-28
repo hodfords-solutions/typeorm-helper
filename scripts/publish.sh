b9fc31e559ac9e0f55b2652367beb0b1ac418d0f
phuong-hodfords <phuong.vt@hodfords.com>
2024-08-28 16:28:31 +0700

    chore: update gitlab ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-npm view @hodfords/typeorm-helper@"$(node p "require('./package.json').version")" version && echo "Package is already published" && exit 0 || true
+npm view @hodfords/typeorm-helper@"$(node -p "require('./package.json').version")" version && echo "Package is already published" && exit 0 || true
 npm install
 npm run build
 cp README.md dist
